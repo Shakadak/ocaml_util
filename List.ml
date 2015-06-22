02da69cf7c2e7f8e432cd04a5d48147097337e98
shakadak <nathanael.pineau@gmail.com>
2015-06-22 22:38:12 +0200

    Some more, nothing descriptive, I was on the hunt to find what I did up till now.

--- List.ml
+++ List.ml
@@ -3,23 +3,42 @@ let rec take n xs = match xs, n with
     | _, 0      -> []
     | x::xs, n  -> x::take (n - 1) xs
 
-let rec take_while f xs = match xs with
+let rec take_while p xs = match xs with
     | []                -> []
-    | x::xs when f x    -> x::take_while f xs
+    | x::xs when p x    -> x::take_while p xs
     | _                 -> []
 
-let rec drop_while f xs = match xs with
+let rec drop_while p xs = match xs with
     | []                -> []
-    | x::xs when f x    -> drop_while f xs
+    | x::xs when p x    -> drop_while p xs
     | xs                -> xs
 
-let span f xs = (take_while f xs, drop_while f xs)
+let span p xs = (take_while p xs, drop_while p xs)
 
-let rec group_by f xs = match xs with
+let rec group_by p xs = match xs with
     | []    -> []
-    | x::xs -> let (ys, zs) = span (f x) xs
-    in (x::ys)::group_by f zs
+    | x::xs -> let (ys, zs) = span (p x) xs
+    in (x::ys)::group_by p zs
 
 let last xs = List.hd @@ List.rev xs
 
 let init xs = List.tl @@ List.rev xs
+
+let rec append xs ys = match xs with
+    | []    -> ys
+    | x::xs -> x::append xs ys
+
+let rec foldl f acc xs = match xs with
+    | []    -> acc
+    | x::xs -> foldl f (f acc x) xs
+
+let rec map f xs = match xs with
+    | []    -> []
+    | x::xs -> f x::map f xs
+
+let rec any p xs = match xs with
+    | []                -> false
+    | x::xs when p x    -> true
+    | _::xs             -> any p xs
+
+let elem x xs = any ((=) x) xs
--- String.ml
+++ String.ml
@@ -7,3 +7,10 @@ let string_of_list string_of_a xs =
 
 let string_of_duple string_of_left string_of_right (l, r) =
     "(" ^ string_of_left l ^ ", " ^ string_of_right r ^ ")"
+
+let string_to_list s =
+    let rec expl i acc =
+        if i < 0
+        then acc
+        else expl (i - 1) (String.get s i :: acc)
+    in expl (String.length s - 1) []
