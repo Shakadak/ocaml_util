208e10e3980252e20dbb86d75011b692fc5b47eb
shakadak <nathanael.pineau@gmail.com>
2015-06-23 19:40:19 +0200

    fake infix operator

--- Function.ml
+++ Function.ml
@@ -2,4 +2,10 @@ let (@.) f g x = f (g x)
 
 let on c f = fun x y -> c (f x) (f y)
 
+(* Infix operators, can be used like `compare /*on*/ fst`.
+** Because of precedence concerne, it is recommended that you surround the
+** composition with parentheses, like so: `(compare /*on*/ fst) x y` *)
+let ( /* ) x y = y x
+and ( */ ) x y = x y
+
 let flip f x y = f y x
