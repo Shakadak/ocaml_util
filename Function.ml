def2fa5027dee39ad9a102ed99b9bbc5f1eefa0c
shakadak <nathanael.pineau@gmail.com>
2015-06-23 20:12:08 +0200

    alternate infix

--- Function.ml
+++ Function.ml
@@ -5,7 +5,9 @@ let on c f = fun x y -> c (f x) (f y)
 (* Infix operators, can be used like `compare /*on*/ fst`.
 ** Because of precedence concerne, it is recommended that you surround the
 ** composition with parentheses, like so: `(compare /*on*/ fst) x y` *)
-let ( /* ) x y = y x
-and ( */ ) x y = x y
+let ( /* ) x y = y x and ( */ ) x y = x y
+
+(* Another possibility, this need further testing *)
+let (>-) x y = (x,y) and (-<) (x,f) y = f x y
 
 let flip f x y = f y x
