52a2d448af231c6f92a2789cb5a8fc2e993964b3
Nathanael <Shakadak@users.noreply.github.com>
2015-06-26 01:30:46 +0200

    Update MyOption.mli

--- MyOption.mli
+++ MyOption.mli
@@ -15,14 +15,23 @@ val fromSome : 'a option -> 'a
 val fromOption : 'a -> 'a option -> 'a
 (** Extract the value of an option, using the default value in case of None *)
 
-headToOption : 'a list -> 'a option
+val headToOption : 'a list -> 'a option
 (** Extract the head of a list, returning None in case of empty list *)
 
-optionToList : 'a option -> 'a list
+val tailToOption : 'a list -> 'a list option
+(* Extract the tail of a list, returning None in case of empty list *)
+
+val lastToOption : 'a list -> 'a option
+(** Extract the last element of a list, returning none in case of empty liste *)
+
+val initToOption : 'a list -> 'a list option
+(** Return a list without its last element, return None in case of empty list *)
+
+val optionToList : 'a option -> 'a list
 (** Extract an option to a singleton, returning an empty list in case of None *)
 
-catOptions : 'a option list -> 'a list
+val catOptions : 'a option list -> 'a list
 (** Construct a list discarding the None values *)
 
-mapOption : ('a -> 'b option) -> 'a list -> 'b list
+val mapOption : ('a -> 'b option) -> 'a list -> 'b list
 (** Construct a list discarding the None values returned *)
