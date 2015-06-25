099770d54aaeb67d142c8ef2cf75f19fcfd44bf2
shakadak <nathanael.pineau@gmail.com>
2015-06-25 14:13:02 +0200

    added partialy applicable not

--- Combinator.ml
+++ Combinator.ml
@@ -10,6 +10,8 @@ let uncurry f (x, y) = f x y
 
 let comparing p = on compare p
 
+let not f = fun x y -> not (f x y)
+
 (* Infix operators, can be used like `compare /*on*/ fst`.
 ** Because of precedence concerne, it is recommended that you surround the
 ** composition with parentheses, like so: `(compare /*on*/ fst) x y` *)
